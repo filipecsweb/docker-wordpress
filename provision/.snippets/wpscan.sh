6478bfd1b00e54bf9fe4fe82b62821bbce4c690a
Filipe Seabra <filipecseabra@gmail.com>
2020-07-09 15:30:59 -0300

    Add wpscan.sh.

---
+++ provision/.snippets/wpscan.sh
@@ -0,0 +1,4 @@
+# Install
+apt update && apt install -y curl && apt install -y ruby-full && gem install bundler && \
+apt install -y build-essential patch ruby-dev zlib1g-dev liblzma-dev && gem install nokogiri && \
+gem install wpscan
