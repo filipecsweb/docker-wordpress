c7cf591e00a25298f6a7b8b55c3163a9a9e8f8e2
Filipe Seabra <filipecseabra@gmail.com>
2020-07-30 16:04:29 -0300

    Update script file: wp-cli.sh.

--- provision/.snippets/wp-cli.sh
+++ provision/.snippets/wp-cli.sh
@@ -1,3 +1,4 @@
 # Generate posts
-curl https://loripsum.net/api/6/medium/decorate/link/ul/ol/bq/headers | \
-wp post generate --count=10 --post_title="Baby shark" --post_type=post --post_status=publish --post_author=1 --post_content
+curl -o .post_content.txt https://loripsum.net/api/6/medium/decorate/link/ul/ol/bq/headers; \
+cat .post_content.txt | \
+wp post generate --count=24 --post_title="Baby shark" --post_type=post --post_status=publish --post_author=1 --post_content
